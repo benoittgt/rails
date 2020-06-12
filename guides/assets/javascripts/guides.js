685ec8a2e69db10596d44947f3fbe11d2db7efe5
Benoit Tigeot <benoit@hopsandfork.com>
2020-06-12 16:06:01 +0200

    e

--- guides/assets/javascripts/guides.js
+++ guides/assets/javascripts/guides.js
@@ -29,21 +29,6 @@
   document.addEventListener("turbolinks:load", function() {
     Prism.highlightAll();
 
-    const textNodeType = 3;
-    function removeWhiteSpaceAfterPrompt(element) {
-      each(element.childNodes, function(childElement) {
-        if (childElement.textContent == " " && childElement.nodeType == textNodeType) {
-          childElement.remove();
-        }
-      })
-    }
-
-    // space after shell prompt is replaced with margin to make shell command
-    // more easily selectable
-    each(document.querySelectorAll("span.token.command"), function(element) {
-      removeWhiteSpaceAfterPrompt(element);
-    });
-
     var guidesMenu = document.getElementById("guidesMenu");
     var guides     = document.getElementById("guides");
 
